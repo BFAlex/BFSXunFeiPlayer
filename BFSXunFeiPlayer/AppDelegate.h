bf64c4d3e450bc210abe95a01575e973a910b81e
GKD <liuling@liulingdemac-mini.local>
2018-12-22 15:53:44 +0800

    V1.0

---
+++ BFSXunFeiPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFSXunFeiPlayer
+//
+//  Created by 刘玲 on 2018/12/22.
+//  Copyright © 2018年 GKD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFSXunFeiPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFSXunFeiPlayer
+//
+//  Created by 刘玲 on 2018/12/22.
+//  Copyright © 2018年 GKD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
